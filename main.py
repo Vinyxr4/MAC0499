08e2972086c811a061031d5eec079870d298df71
vinyxr4 <viniciuspessoaduarte54@gmail.com>
2018-06-04 22:44:38 -0300

    Fixes frenq related to sampleRate

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import audio.audioHandler as audio
 import supression.spectralSubtraction as specSub
 import noisy.noisy as noisy
 import tests.tests as test
+import numpy as np
 
 testAudio = 'history.wav'
 
-suppressedAudio, noisyAudio, sampleRate = test.spectral(testAudio, useEstimate=True)
+suppressedAudio, noisyAudio, sampleRate, noise = test.spectral(testAudio, useEstimate=True)
 
+print('Noisy audio:')
 audio.play(noisyAudio, sampleRate)
+
+print('Spectral supressed audio:')
 audio.play(suppressedAudio, sampleRate)
+
+audioArray, sampleRate, encoding = audio.getData(testAudio)
+
+audio.play(noise, sampleRate)
+
+print('Signal domain suppresed audio:')
+audio.play(noisyAudio - noise, sampleRate)
