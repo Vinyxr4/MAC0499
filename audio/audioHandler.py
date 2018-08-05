6c1ef85262223f55a63b5a26b9d5facc76e7fb11
vinyxr4 <vinicius@cobli.co>
2018-08-05 16:53:14 -0300

    Feature to save file as

--- audio/audioHandler.py
+++ audio/audioHandler.py
@@ -7,3 +7,9 @@ def getData (audioPath):
 
 def play (audioArray, sampleRate):
     audiolab.play(audioArray, sampleRate)
+
+def saveAs(audioArray, sampleRate, fileName='test.wav'):
+    s = audiolab.Sndfile(fileName, mode='w', format=audiolab.Format(), channels=1, samplerate=sampleRate)
+    s.write_frames(audioArray)
+
+    print('Saved as: {}'.format(fileName))
