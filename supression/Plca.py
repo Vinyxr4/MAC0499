a567b20552a68e28b5f696a41b7a5e523aafe7fd
vinyxr4 <vinicius@cobli.co>
2018-08-07 23:12:35 -0300

    Update functions created

--- supression/Plca.py
+++ supression/Plca.py
@@ -55,7 +55,50 @@ def update_priori_s(priories_sz_given_f, spectrum, s):
 
     return result
 
-def test_first_update(s, z, f, iterations):
+def update_priori_z_given_s(priories_sz_given_f, spectrum, s, z):
+    dividend = 0.0
+
+    for f_prime in range(len(priories_sz_given_f[0][0])):
+        dividend += spectrum[f_prime] * priories_sz_given_f[s][z][f_prime]
+
+    divisor = 0.0
+
+    for f_prime in range(len(priories_sz_given_f[0][0])):
+        inner_divisor = 0.0
+        
+        for z_prime in range(len(priories_sz_given_f[0])):
+            inner_divisor += priories_sz_given_f[s][z_prime][f_prime]
+
+        divisor += spectrum[f_prime] * inner_divisor
+
+    epslon = 0.0
+    result = dividend / divisor if divisor > epslon else 0.0
+
+    return result
+
+def update_priori_f_given_sz(all_priories_sz_given_f, all_spectrums, s, z, f, turn):
+    dividend = 0.0
+
+    for t_prime in range(turn + 1):
+        dividend += all_spectrums[t_prime][f] * all_priories_sz_given_f[t_prime][s][z][f]
+
+    divisor = 0.0
+
+    for f_prime in range(len(all_priories_sz_given_f[0][0][0])):
+        inner_divisor = 0.0
+        
+        for t_prime in range(turn + 1):
+            inner_divisor += all_priories_sz_given_f[t_prime][s][z][f_prime] * all_spectrums[t_prime][f_prime]
+
+        divisor += inner_divisor
+
+    epslon = 0.0
+    result = dividend / divisor if divisor > epslon else 0.0
+
+    return result
+
+
+def test_first_update(s, z, f):
     priories_s = np.zeros((s))
     priories_z_given_s = np.zeros((s, z))
     priories_f_given_sz = np.zeros((s, z, f))
@@ -72,7 +115,7 @@ def test_first_update(s, z, f, iterations):
 
     print (val)
 
-def test_second_update(s, z, f, iterations):
+def test_second_update(s, z, f):
     priories_sz_given_f = np.zeros((s, z, f))
 
     spectrum = np.zeros((f))
@@ -87,5 +130,38 @@ def test_second_update(s, z, f, iterations):
 
     print (val)
 
-# test_first_update(2, 3, 1, 10)
-# test_second_update(2, 1, 1, 10)
+def test_third_update(s, z, f):
+    priories_sz_given_f = np.zeros((s, z, f))
+
+    spectrum = np.zeros((f))
+
+    spectrum[:] = 1.0 / f
+
+    for s_prime in range(s):
+        for z_prime in range(z):
+            priories_sz_given_f[s_prime][z_prime][:] = 1.0 / f
+
+    val = update_priori_z_given_s(priories_sz_given_f, spectrum, 0, 0)
+
+    print (val)
+
+def test_fourth_update(s, z, f, turns):
+    all_priories_sz_given_f = np.zeros((turns, s, z, f))
+
+    all_spectrums = np.zeros((turns, f))
+
+    for t_prime in range(turns):
+        all_spectrums[t_prime][:] = 1.0 / f
+    
+        for s_prime in range(s):
+            for z_prime in range(z):
+                all_priories_sz_given_f[t_prime][s_prime][z_prime][:] = 1.0 / f
+
+    val = update_priori_f_given_sz(all_priories_sz_given_f, all_spectrums, 0, 0, 0, 0)
+
+    print (val)
+
+# test_first_update(2, 3, 1)
+# test_second_update(2, 1, 1)
+# test_third_update(10, 2, 10)
+test_fourth_update(1, 1, 2, 30)
