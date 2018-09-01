806049c1ab3c27ac53ddbc30ccac4152b97be6c3
vinyxr4 <vinicius@cobli.co>
2018-09-01 13:05:15 -0300

    Cli working for flms

--- app/cli/cli.py
+++ app/cli/cli.py
@@ -1,6 +1,7 @@
 import sys
 from audio import audioHandler
 from app.supression.spectralSubtraction import spectralSubtraction
+from app.supression.fastLMS import fastLMS
 
 def throwError(error):
     errorString = ''
@@ -13,6 +14,9 @@ def throwError(error):
 
     if error in ['noNoisyPath', 'invalidNoisyPath']:
         errorString = 'É necessário especificar o arquivo de áudio a filtrar!'
+
+    if error in ['noCleanPath']:
+        errorString = 'É necessário especificar a amostra de áudio sem ruído!'
 
     if error in ['noNoisePath', 'invalidNoisePath']:
         errorString = 'É necessário especificar o ruído a ser filtrado!'
@@ -31,13 +35,86 @@ def throwError(error):
 
     if error in ['invalidSavePath']:
         errorString = 'É ncessário especificar o nome utilizado para salvar o áudio suprimido!'
+
+    if error in ['notEnoughM']:
+        errorString = 'É necessário especificar pelo menos 100 para o número de blocos!'
+
+    if error in ['notEnoughforgetness']:
+        errorString = 'É necessário especificar a taxa de esquecimento entre 0.0 e 1.0!'
+
+    if error in ['notEnoughstep']:
+        errorString = 'É necessário especificar o passo entre 0.0 e 1.0!'
     
     raise ValueError(errorString)
+
+def buildStringArgument(argument, args, options, optionName):
+    if argument in args:
+        try:
+            index = args.index(argument) + 1
+        except:
+            throwError('invalid{}'.format(optionName))
+
+        try:
+            if args[index].startswith('--'):
+                throwError('invalid{}'.format(optionName))
+        except:
+            throwError('invalid{}'.format(optionName))
+
+        options[optionName] = args[index]
+
+    return options
+
+def buildIntArgument(argument, args, options, optionName, minVal):
+    if argument in args:
+        try:
+            index = args.index(argument) + 1
+        except:
+            throwError('notEnough{}'.format(optionName))
+
+        try:
+            if args[index].startswith('--'):
+                throwError('notEnough{}'.format(optionName))
+        except:
+            throwError('notEnough{}'.format(optionName))
+
+        try:
+            options[optionName] = int(args[index])
+        except:
+            throwError('notAnInt')
+        
+        if options[optionName] < minVal:
+            throwError('notEnough{}'.format(optionName))
+
+    return options
+
+def buildFloatArgument(argument, args, options, optionName, maxVal):
+    if argument in args:
+        try:
+            index = args.index(argument) + 1
+        except:
+            throwError('notEnough{}'.format(optionName))
+
+        try:
+            if args[index].startswith('--'):
+                throwError('notEnough{}'.format(optionName))
+        except:
+            throwError('notEnough{}'.format(optionName))
+
+        try:
+            options[optionName] = float(args[index])
+        except:
+            throwError('notAnInt')
+        
+        if options[optionName] > maxVal:
+            throwError('notEnough{}'.format(optionName))
+
+    return options
 
 def argumentsParser(args):
     METHOD = '--mtd'
     NOISYPATH = '--audioPath'
     SAVE = '--save'
+    NOESTIMATE = '--noisePath'
     METHODS = ['specSub', 'flms', 'plca']
     
     if METHOD not in args:
@@ -65,19 +142,20 @@ def argumentsParser(args):
 
     options = {}
 
+    options['noisePath'] = ''
+    options['cleanPath'] = ''
+    options['save'] = 'suppressed.wav'
+
     if mtdUsed == METHODS[0]:
         ESTIMATE = '--estimate'
-        NOESTIMATE = '--noisePath'
         SPLITRATE = '--blocks'
         PROCESSES = '--numProc'
 
         options['numProcesses'] = 1
         options['splitRate'] = 1
         options['estimate'] = False
-        options['save'] = 'suppressed.wav'
         options['estimateDuration'] = 100
-        options['noisePath'] = ''
-
+        
         if SAVE in args:
             try:
                 savePathIndex = args.index(SAVE) + 1
@@ -144,9 +222,23 @@ def argumentsParser(args):
             options['splitRate'] = args[splitRateIndex]
 
     elif mtdUsed == METHODS[1]:
+        M = '--m'
+        STEP = '--step'
+        FORGETNESS = '--fgt'
+        CLEAN = '--cleanPath'
+
         options['M'] = 100
         options['step'] = 0.1
         options['forgetness'] = 0.9
+
+        if CLEAN not in args:
+            throwError('noCleanPath')
+
+        options = buildStringArgument(CLEAN, args, options, 'cleanPath')
+        options = buildStringArgument(SAVE, args, options, 'save')
+        options = buildIntArgument(M, args, options, 'M', 100)
+        options = buildFloatArgument(FORGETNESS, args, options, 'forgetness', 1.0)
+        options = buildFloatArgument(STEP, args, options, 'step', 1.0)
 
     return mtdUsed, noisyAudioUsed, options
         
@@ -156,9 +248,9 @@ if __name__ == '__main__':
 
     print(methodUsed, audioPath, options)
 
-    if methodUsed == 'specSub':
-        audioPath = audioPath
+    audioPath = audioPath
 
+    if methodUsed == 'specSub':
         noisePath = options['noisePath']
         milliSeconds = int(options['estimateDuration'])
         useEstimate = bool(options['estimate'])
@@ -167,5 +259,14 @@ if __name__ == '__main__':
 
         suppressed, sampleRate, elapsed = spectralSubtraction.spectral(audioPath, noisePath, useEstimate, milliSeconds, processes, splitRate)
 
-        print('Tempo de execução: {} segundos'.format(elapsed))
-        audioHandler.saveAs(suppressed, sampleRate, options['save'])
+    elif methodUsed == 'flms':
+        cleanPath = options['cleanPath']
+        M = int(options['M'])
+        step = float(options['step'])
+        forget = float(options['forgetness'])
+
+        suppressed, sampleRate, elapsed = fastLMS.fastlms(audioPath, cleanPath, M, step, forget)
+
+    print('Tempo de execução: {} segundos'.format(elapsed))
+    audioHandler.saveAs(suppressed, sampleRate, options['save'])
+    
